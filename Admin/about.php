c7dcc27f91ac1dacef134a5ae237d4534716cad5
Atanasius01 <atanasius256@gmail.com>
2023-04-04 23:17:12 +0700

    Website native php

---
+++ Admin/about.php
@@ -0,0 +1,15 @@
+<div class="px-4 py-5 my-5 text-center">
+    <img class="rounded d-block mx-auto mb-4" src="assets/img/atan.jpg" alt="" width="100" height="100">
+    <h1 class="display-5 fw-bold text-body-emphasis">About Me</h1>
+    <div class="col-lg-6 mx-auto">
+      <p class="lead mb-4">Atanasius Jales Putra Bagaskara  </p>
+      <p class="lead mb-4">Mahasiswa Aktif Semester 6 Universitas Pendidikan Indonesia, Aktif mengikuti Program MSIB Batch 4 FUll Stack Web Developer</p>
+      <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+        <a href="https://www.youtube.com/"><button type="button" class="btn btn-primary btn-lg px-4 gap-3" fdprocessedid="85aqup">Curriculum Vitae</button></a>
+      </div>
+      
+    </div>
+  </div>
+  <?php
+include_once 'bottom.php';
+?>
